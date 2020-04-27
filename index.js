dd02e3975c051880c9ee57d1b0062ab4aa6b8ba7
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-27 14:51:54 -0400

    init server, set up helmet dependency

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require("./server.js");
+
+const PORT = 7999;
+
+server.listen(PORT, () => {
+  console.log(`\n API running on ${PORT} \n`);
+});
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const helmet  = require('helmet');
 
 const server = express();
+
+server.use(helmet());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
