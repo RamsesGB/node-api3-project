a96e2f29ca23991249b229f775e0131d3fb8a66e
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-27 20:19:33 -0400

    wrote GET endpoints for user router

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet  = require('helmet');
 
 const server = express();
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 server.use(express.json());
 server.use(helmet());
@@ -9,6 +11,9 @@ server.use(helmet());
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use('/user', userRouter);
+server.use('/post', postRouter);
 
 //custom middleware
 
