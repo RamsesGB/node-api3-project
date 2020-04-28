bb16f81d9cbc47a476450aa3b215d7fd46c54da5
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-27 21:52:25 -0400

    created endpoints for pos router

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,55 @@
 const express = require('express');
 
 const router = express.Router();
+const Post = require('./postDb.js');
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Post.get()
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "Server error. Unable to retrieve posts"})
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+
+  Post.getById( id )
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    res.status(500).json({message: "Server error. Unable to retrieve posts"})
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+
+  Post.remove( id )
+  .then(deletedPost => {
+    res.status(200).json(deletedPost)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({message: "Server error. Unable to delete post"})
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params
+
+  Post.update( id, req.body)
+  .then(updatedPost => {
+    res.status(200).json(updatedPost)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({message: "Server error. Unable to update post"})
+  });
 });
 
 // custom middleware
