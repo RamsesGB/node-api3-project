1a431f63e6867327db2f99e06f48e771dcc63692
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-29 15:24:57 -0400

    refactoring user POST enpoint while debugging validateUser middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,19 +3,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("./userDb.js");
 
-router.post("/", (req, res) => {
-  User.insert(req.body)
-    .then(createdUser => {
-      res.status(201).json(createdUser)
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ message: "Server error. Unable to create user"})
-    });
+router.post("/", validateUser, (req, res) => {
+  res.status(201).json(createdUser);
 });
 
-router.post("/:id/posts", (req, res) => {
-});
+router.post("/:id/posts", (req, res) => {});
 
 router.get("/", (req, res) => {
   User.get()
@@ -32,10 +24,10 @@ router.get("/", (req, res) => {
 
 router.get("/:id", validateUserId, (req, res) => {
   const { id } = req.params;
-  
+
   User.getById(id)
     .then((user) => {
-      console.log(` from GET /:id endpoint \n${JSON.stringify(user)}`)
+      console.log(` from GET /:id endpoint \n${JSON.stringify(user)}`);
       res.status(200).json(user);
     })
     .catch((err) => {
@@ -78,39 +70,50 @@ router.put("/:id", (req, res) => {
   const { id } = req.params;
 
   User.update(id, req.body)
-  .then(updatedUser => {
-    res.status(200).json(updatedUser)
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ message: "Server error. Unable to update user" })
-  });
+    .then((updatedUser) => {
+      res.status(200).json(updatedUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Server error. Unable to update user" });
+    });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  const {id} = req.params
+  const { id } = req.params;
 
-  User.getById( id )
-  .then( user => {
-    console.log(` from validation mid-ware`, user)
-    if (user) {
-      req.user = user;
-      next();
-    } else {
-      res.status(400).json({ message: "invalid user id" })
-    }
-  })
-  .catch( err => {
-    console.log(err)
-    res.status(500).json({ message: "server error" })
-  });
+  User.getById(id)
+    .then((user) => {
+      console.log(` from validation mid-ware`, user);
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid user id" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "server error" });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
-  next();
+  User.insert(req.body)
+    .then((createdUser) => {
+      console.log(`from user validate mid-ware \n`, createdUser);
+      if (createdUser) {
+        next();
+      } else {
+        res.status(400).json({ message: "missing user data" });
+      }
+    })
+    .catch((err) => {
+      console.log(`Mid-ware Error msg = `, err);
+      res.status(500).json({ message: "server error" });
+    });
 }
 
 function validatePost(req, res, next) {
